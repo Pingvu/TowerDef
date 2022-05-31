6fdd802fc4eef36cab8ae2d7618ebac073a5f0f7
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-05-31 13:42:15 +0200

    Update Enemies.cs

--- tower defense (1)/Assets/script/Enemies.cs
+++ tower defense (1)/Assets/script/Enemies.cs
@@ -3,6 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemies : MonoBehaviour
-{
+{ //creating a list for refence in other script
    public static List<GameObject> enemies = new List<GameObject>() ;
 }
