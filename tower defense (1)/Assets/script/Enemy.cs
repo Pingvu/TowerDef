78f959ffb3dbb5ca29c3a6629f0af44ca1080901
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-05-31 13:34:59 +0200

    Update Enemy.cs

--- tower defense (1)/Assets/script/Enemy.cs
+++ tower defense (1)/Assets/script/Enemy.cs
@@ -3,25 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-{
+{ //creating variables
   [SerializeField] private float enemyHealth;
    [SerializeField]private float movementSpeed;
    
 
    private GameObject targetTile;
-   private void Awake()
+   private void Awake() 
    {
       Enemies.enemies.Add(gameObject);
    }
-   private void Start()
+   private void Start() //spawn the enemy
    {
        initializeEnemy();
    }
-   private void initializeEnemy()
+   private void initializeEnemy() //Enemy will spawn in the startTile
    {
      targetTile = MapGenerator.startTile;
    }
-   public void takeDamage (float amount)
+   public void takeDamage (float amount) //if the enemy health reached 0, do the method die
    {
       enemyHealth -= amount;
       if (enemyHealth <= 0)
@@ -30,18 +30,18 @@ public class Enemy : MonoBehaviour
       }
 
    }
-   private void die () {
+   private void die () { //kill the enemy
       Enemies.enemies.Remove(gameObject);
       Destroy(transform.gameObject);
    }
    
 
-   private void moveEnemy()
+   private void moveEnemy() //enemy will move according to the variable movementSpeed
    {
      transform.position = Vector3.MoveTowards(transform.position, targetTile.transform.position, movementSpeed * Time.deltaTime);
    }
    private void checkPosition()
-   {
+   { //check the enemy position and ensure it will move along the path
    if (targetTile != null && targetTile != MapGenerator.endTile)
       {
          float distance = (transform.position - targetTile.transform.position).magnitude;
@@ -52,10 +52,9 @@ public class Enemy : MonoBehaviour
          }
       }
    }
-   private void Update () 
+   private void Update () //Check enemy position and then move it along the path
    {
    checkPosition();
    moveEnemy();
-   takeDamage (0);
    }
 }
