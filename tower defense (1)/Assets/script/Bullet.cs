83f8f4014b19391e83f2bbc63629d25d86f64c06
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-05-31 14:08:45 +0200

    add comment

--- tower defense (1)/Assets/script/Bullet.cs
+++ tower defense (1)/Assets/script/Bullet.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-  private void Start()
+  private void Start() //delete the bullet after 10s
   {
     Destroy(gameObject, 10f);
   }
-  private void OnCollisionEnter2D(Collision2D collision){
+  private void OnCollisionEnter2D(Collision2D collision){ // delete the bullet when it collide
     Destroy(gameObject);
   }
   private void Update ()
   {   
     
-      transform.position += transform.right * 0.25f;
+      transform.position += transform.right * 0.25f; // move the bullet forward
   }
 }
